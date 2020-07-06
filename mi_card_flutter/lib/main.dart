ea806336c1e8ae156094d21a3208df43149fd910
nkosigumede <nkosi.gumede@easypay.co.za>
2020-07-06 08:55:22 +0200

    Watched up to tutorial 7

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -9,18 +9,70 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50,
-              backgroundColor: Colors.white,
-            )
-          ],
-        )),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            radius: 60,
+            backgroundImage: AssetImage('images/nkosi.jpg'),
+          ),
+          Text('Nkosi Gumede',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              )),
+          Text(
+            'Flutter Developer',
+            style: TextStyle(
+              fontFamily: 'Source Sans Pro',
+              color: Colors.teal.shade100,
+              fontSize: 20,
+              letterSpacing: 2.5,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 20,
+            width: 150,
+            child: Divider(
+              color: Colors.teal.shade100,
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
+              leading: Icon(Icons.phone, color: Colors.teal),
+              title: Text(
+                '+44 123 456 789',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+          Card(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+            child: ListTile(
+              leading: Icon(Icons.email, color: Colors.teal),
+              title: Text(
+                'nkosi@email.com',
+                style: TextStyle(
+                  color: Colors.teal.shade900,
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      )),
+    ));
   }
 }
